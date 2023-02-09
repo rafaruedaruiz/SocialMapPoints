ac982ff94a1a0842337e449ea6b44c04f317fb67
Rafa Rueda Ruiz <48415466+rafaruedaruiz@users.noreply.github.com>
2023-02-09 17:20:32 +0100

    Mapa en Home

--- app/src/main/java/com/rafa/socialmappoints/HomeActivity.kt
+++ app/src/main/java/com/rafa/socialmappoints/HomeActivity.kt
@@ -3,6 +3,9 @@ package com.rafa.socialmappoints
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -11,10 +14,14 @@ enum class ProviderType{
     GOOGLE
 }
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        createFragment()
 
         // Setup
         val bundle = intent.extras
@@ -27,6 +34,15 @@ class HomeActivity : AppCompatActivity() {
         prefs.putString("email", email)
         prefs.putString("provider", provider)
         prefs.apply()
+    }
+
+    private fun createFragment(){
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 
     private fun setup(email: String, provider: String){
