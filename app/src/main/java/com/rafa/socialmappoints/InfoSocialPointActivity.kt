b18fc9e75e9b4aa07aaad536aa57cb2531b48fe8
Rafa Rueda Ruiz <48415466+rafaruedaruiz@users.noreply.github.com>
2023-02-16 05:01:59 +0100

    funcionalidad comoLlegar a un punto

--- app/src/main/java/com/rafa/socialmappoints/InfoSocialPointActivity.kt
+++ app/src/main/java/com/rafa/socialmappoints/InfoSocialPointActivity.kt
@@ -2,6 +2,7 @@ package com.rafa.socialmappoints
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
@@ -13,8 +14,11 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_info_social_point.*
 
 class InfoSocialPointActivity : AppCompatActivity(){
+
+    private lateinit var socialPoint: SocialPoint
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +32,17 @@ class InfoSocialPointActivity : AppCompatActivity(){
         val editButton = findViewById<ImageButton>(R.id.editButton)
 
         val database = FirebaseDatabase.getInstance().reference
-        val socialPoint = socialPointId?.let { database.child("social_points").child(socialPointId) }   // .child(it)
-        socialPoint?.addValueEventListener(object : ValueEventListener {
+        val socialPointRef = socialPointId?.let { database.child("social_points").child(socialPointId) }!!
+        socialPointRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val socialPoint = dataSnapshot.getValue(SocialPoint::class.java)
+                socialPoint = dataSnapshot.getValue(SocialPoint::class.java)!!
                 if (socialPoint != null) {
                     titleTextView.text = socialPoint.title
                     descriptionTextView.text = socialPoint.description
                     if(FirebaseAuth.getInstance().currentUser?.uid.toString() == socialPoint.userID){
                         deleteButton.visibility = View.VISIBLE
                         editButton.visibility = View.VISIBLE
-                    }else {
+                    } else {
                         deleteButton.visibility = View.GONE
                         editButton.visibility = View.GONE
                     }
@@ -56,13 +60,13 @@ class InfoSocialPointActivity : AppCompatActivity(){
                 builder.setTitle("Eliminar punto")
                 builder.setMessage("¿Está seguro de que quiere eliminar este punto social?")
                 builder.setPositiveButton("Eliminar") { _, _ ->
-                    socialPoint.removeValue()
+                    socialPointRef.removeValue()
                     Toast.makeText(this, "Punto Social eliminado con éxito", Toast.LENGTH_SHORT).show()
                     val homeIntent = Intent(this, HomeActivity::class.java)
                     startActivity(homeIntent)
                 }
                 builder.setNegativeButton("Cancelar") { _, _ ->
-                   // El usuario ha cancelado (no hacer nada)
+                    // El usuario ha cancelado (no hacer nada)
                 }
                 builder.create().show()
             }
@@ -74,5 +78,10 @@ class InfoSocialPointActivity : AppCompatActivity(){
             startActivity(editIntent)
         }
 
+        comoLlegarButton.setOnClickListener {
+            val uri = Uri.parse("google.navigation:q=${socialPoint.latitude},${socialPoint.longitude}")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
