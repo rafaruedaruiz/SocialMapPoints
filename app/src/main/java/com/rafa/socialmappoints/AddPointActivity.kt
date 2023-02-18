c8200ea7f567de17d0cb54f6db714a3d1c898a1d
Rafa Rueda Ruiz <48415466+rafaruedaruiz@users.noreply.github.com>
2023-02-18 18:05:20 +0100

    imagenes se suben a storage

--- app/src/main/java/com/rafa/socialmappoints/AddPointActivity.kt
+++ app/src/main/java/com/rafa/socialmappoints/AddPointActivity.kt
@@ -1,6 +1,7 @@
 package com.rafa.socialmappoints
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
@@ -10,16 +11,34 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_add_point.*
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+
+
+
 
 class AddPointActivity : AppCompatActivity() {
 
     private lateinit var addPointButton: Button
+    private lateinit var imageButton: Button
+    private lateinit var imageUriList: MutableList<Uri>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_point)
 
         addPointButton = findViewById(R.id.addPointButton)
+        imageButton = findViewById(R.id.addImageButton)
+        imageUriList = mutableListOf()
+
+        imageButton.setOnClickListener {
+            // abre galeria
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            startActivityForResult(Intent.createChooser(intent, "Selecciona imágenes"), REQUEST_CODE_SELECT_IMAGES)
+        }
 
         addPointButton.setOnClickListener {
             val userID = FirebaseAuth.getInstance().currentUser?.uid.toString()
@@ -45,5 +64,37 @@ class AddPointActivity : AppCompatActivity() {
         val reference = database.getReference("social_points")
         val key = reference.push().key
         reference.child(key!!).setValue(socialPoint)
+            .addOnSuccessListener {
+                // Sube las imágenes a Firebase Storage
+                uploadImages(key)
+            }
+    }
+
+    private fun uploadImages(socialPointID: String) {
+        val storage = Firebase.storage
+        val reference = storage.reference.child("images/$socialPointID")
+        for (i in 0 until imageUriList.size) {
+            val imageReference = reference.child("image_$i")
+            imageReference.putFile(imageUriList[i])
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SELECT_IMAGES && resultCode == RESULT_OK) {
+            // Guarda las URIs de las imágenes seleccionadas
+            if (data?.clipData != null) {
+                val clipData = data.clipData!!
+                for (i in 0 until clipData.itemCount) {
+                    imageUriList.add(clipData.getItemAt(i).uri)
+                }
+            } else if (data?.data != null) {
+                imageUriList.add(data.data!!)
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_SELECT_IMAGES = 1
     }
 }
